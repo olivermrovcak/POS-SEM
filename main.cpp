62caa9b8ceca85add9b923b3014ea21e10bf1650
Oliver Mrovčák <oliver.mrovcak@prosoft.sk>
2024-01-04 22:46:41 +0100

    added menu option for ftp dowlonad

--- main.cpp
+++ main.cpp
@@ -24,8 +24,6 @@ void initialize_ncurses() {
 int main() {
     initialize_ncurses();
 
-    Download download("FTP", "klokanek.endora.cz", "/Users/olivermrovcak/test.txt", "/web/style.css", "olivergg", "Heslo5.", 1);
-
     // Create main menu and submenus
     Menu mainMenu("Choose protocol\n", {"FTP", "FTPS", "HTTP", "HTTPS", "Exit"});
     Menu ftpMenu("FTP Options\n", {"Option 1", "Option 2", "Back"});
@@ -35,6 +33,9 @@ int main() {
     mainMenu.setAction(4, [&]() { endwin(); exit(0); });
 
     ftpMenu.setAction(2, [&]() { mainMenu.display(); });
+    ftpMenu.setAction(0, [&]() {
+        Download download("FTP", "klokanek.endora.cz", "/Users/olivermrovcak/test.txt", "/web/style.css", "olivergg", "Heslo5.", 1);
+    });
 
     // Display the main menu
     mainMenu.display();
